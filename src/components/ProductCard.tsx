b1c9a4d357a270221d983c7ea082c11dbe92a0d2
Ayoub-Fatmi <fatmiayoub04@gmail.com>
2025-04-09 16:32:47 +0100

    edit of addToCart button in productCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -72,7 +72,7 @@ function ProductCard({
             {isInCart ? (
               <div className="flex items-center gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
-                {cartQuantity ? `Quantity:  ${cartQuantity} • add 1 to cart` : "Add to Cart"}
+                {`${cartQuantity} in cart • add 1 more`}
               </div>
             ) : (
               <>
