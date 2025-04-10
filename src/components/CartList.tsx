07d9bc3fe9655df5599b6711612670a5007112f1
Ayoub-Fatmi <Fatmiayoub04@gmail.com>
2025-04-10 20:33:21 +0100

    fix the responsivness of the cartList

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -12,8 +12,8 @@ function CartList({ cart }: CartListProps) {
     <div className="lg:w-2/3">
       <div className="bg-white rounded-lg shadow overflow-hidden">
         <div className="hidden md:grid grid-cols-12 bg-gray-100 p-4 font-medium text-gray-700">
-          <div className="col-span-5">Product</div>
-          <div className="col-span-2 text-center">Price</div>
+          <div className="col-span-4">Product</div>
+          <div className="col-span-3 text-center">Price</div>
           <div className="col-span-3 text-center">Quantity</div>
           <div className="col-span-2 text-right">Subtotal</div>
         </div>
